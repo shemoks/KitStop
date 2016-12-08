a253c5d95a57f1a46121a085a00d5dfa1c8df7a9
Mac <MacMini@Mac-mini-Mac.local>
2016-12-08 18:32:13 +0200

    create design for sign up screen

---
+++ KitStop/Common/Views/CustomButton.swift
@@ -0,0 +1,58 @@
+//
+//  CustomButton.swift
+//  KitStop
+//
+//  Created by Mac on 12/8/16.
+//  Copyright © 2016 MOZI Development. All rights reserved.
+//
+
+import UIKit
+import Foundation
+@IBDesignable class CustomButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor.white
+        self.tintColor = UIColor(red: (107/255.0), green: (110/255.0), blue: (112/255.0), alpha: 1.0)
+        self.titleLabel?.font = UIFont(name: "SFUIText-Regular", size: 17)
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+
+    @IBInspectable var backGroundColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.backgroundColor!)
+        }
+        set {
+            layer.backgroundColor = newValue.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+}
