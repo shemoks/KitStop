b89c03ddb36a94b402a9b18bd49be5edcedbd51a
Oksana Shemshur <o.shemshur@mozidev.com>
2017-02-07 13:49:24 +0200

    modified view oner information

--- KitStop/Common/CalculationForOwner.swift
+++ KitStop/Common/CalculationForOwner.swift
@@ -8,31 +8,33 @@
 
 import Foundation
 class CalculationForOwner: TransportPost {
-var post: Post?
+    var post: Post?
 
-fileprivate let dataManagerRates: CreateForSaleServiceProtocol
-init(dataManagerRates: CreateForSaleServiceProtocol) {
-    self.dataManagerRates = dataManagerRates
-}
+    fileprivate let dataManagerRates: CreateForSaleServiceProtocol
+    init(dataManagerRates: CreateForSaleServiceProtocol) {
+        self.dataManagerRates = dataManagerRates
+    }
 
-convenience init() {
-    self.init(dataManagerRates: CreateForSaleService())
-}
-    func getInfo(result: @escaping (RatesModel) -> ()) {
+    convenience init() {
+        self.init(dataManagerRates: CreateForSaleService())
+    }
+    func getInfo(result: @escaping (RatesModel?) -> ()) {
         dataManagerRates.getRates { [weak self]
             success, error, rates in
             if error == nil {
                 result(rates!)
+            } else {
+                result(nil)
             }
         }
 
     }
-    
+
     func returnPost(post: Post) {
         self.post = post
     }
 
-    func getValuesForView(post: Post, rates: RatesModel) -> PriceModel {
+    func getValuesForView(post: Post, rates: RatesModel) -> PriceModel? {
         var resultRate = 0.00
         var stringRate = ""
         var price = "0.00"
@@ -50,21 +52,26 @@ convenience init() {
                 price = index.textValue
             }
         }
-
-        let transactionPrice = Double(price)! * (rates.transactionPercent)/100
-        let transactionRatePrice = Double(price)! * (rates.transactionRate)/100
-        let kitStopPrice = Double(price)! * (rates.kitStopFee)/100
-        let userPrice = Double(price)! - transactionPrice - transactionRatePrice - kitStopPrice - resultRate
-        let priceModel = PriceModel(weight: "Shipping UDSP \(stringRate):", weightRate: "$\(String(format: "%.2f", resultRate))" ,startingPrice: "$\(price.formattedDouble(decimalPlaces: 2))", transactionPrice: "$\(String(format: "%.2f", transactionPrice))", transactionRatePrice: "$\(String(format: "%.2f", transactionRatePrice))", kitStopPrice: "$\(String(format: "%.2f", kitStopPrice))", finalPrice: "$\(String(format: "%.2f", userPrice))")
-
-        return priceModel
+        if price != "" {
+            let transactionPrice = Double(price)! * (rates.transactionPercent)/100
+            let transactionRatePrice = Double(price)! * (rates.transactionRate)/100
+            let kitStopPrice = Double(price)! * (rates.kitStopFee)/100
+            let userPrice = Double(price)! - transactionPrice - transactionRatePrice - kitStopPrice - resultRate
+            let priceModel = PriceModel(weight: "Shipping UDSP \(stringRate):", weightRate: "$\(String(format: "%.2f", resultRate))" ,startingPrice: "$\(price.formattedDouble(decimalPlaces: 2))", transactionPrice: "$\(String(format: "%.2f", transactionPrice))", transactionRatePrice: "$\(String(format: "%.2f", transactionRatePrice))", kitStopPrice: "$\(String(format: "%.2f", kitStopPrice))", finalPrice: "$\(String(format: "%.2f", userPrice))")
+            return priceModel
+        }
+        return nil
     }
 
     func calculate (resultValue: @escaping (PriceModel) -> ()) {
-        var priceModel = PriceModel(weight: "0.00", weightRate: "0.00", startingPrice: "0.00", transactionPrice: "0.00", transactionRatePrice: "0.00", kitStopPrice: "0.00", finalPrice: "0.00")
+        var priceModel = PriceModel(weight: "Shipping:", weightRate: "0.00", startingPrice: "0.00", transactionPrice: "0.00", transactionRatePrice: "0.00", kitStopPrice: "0.00", finalPrice: "0.00")
         self.getInfo { result in
-           priceModel =  self.getValuesForView(post: self.post!, rates: result)
-           resultValue(priceModel)
+            if result != nil {
+                priceModel =  self.getValuesForView(post: self.post!, rates: result!)!
+                resultValue(priceModel)
+            } else {
+                resultValue(priceModel)
+            }
         }
     }
 }
